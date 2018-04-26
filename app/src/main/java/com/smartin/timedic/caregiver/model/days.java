e0b1f80a4e984242636f243758ae8d1dd039f880
Cakra Adipura W <adipurapunya@gmail.com>
2018-04-27 00:22:12 +0700

    Add order

---
+++ app/src/main/java/com/smartin/timedic/caregiver/model/days.java
@@ -0,0 +1,73 @@
+package com.smartin.timedic.caregiver.model;
+
+/**
+ * Created by adipura on 4/8/2018.
+ */
+
+public class days {
+
+    private Boolean monday;
+    private Boolean tuesday;
+    private Boolean wednesday;
+    private Boolean thursday;
+    private Boolean friday;
+    private Boolean saturday;
+    private Boolean sunday;
+
+
+    public Boolean getMonday() {
+        return monday;
+    }
+
+    public void setMonday(Boolean monday) {
+        this.monday = monday;
+    }
+
+    public Boolean getTuesday() {
+        return tuesday;
+    }
+
+    public void setTuesday(Boolean tuesday) {
+        this.tuesday = tuesday;
+    }
+
+    public Boolean getWednesday() {
+        return wednesday;
+    }
+
+    public void setWednesday(Boolean wednesday) {
+        this.wednesday = wednesday;
+    }
+
+    public Boolean getThursday() {
+        return thursday;
+    }
+
+    public void setThursday(Boolean thursday) {
+        this.thursday = thursday;
+    }
+
+    public Boolean getFriday() {
+        return friday;
+    }
+
+    public void setFriday(Boolean friday) {
+        this.friday = friday;
+    }
+
+    public Boolean getSaturday() {
+        return saturday;
+    }
+
+    public void setSaturday(Boolean saturday) {
+        this.saturday = saturday;
+    }
+
+    public Boolean getSunday() {
+        return sunday;
+    }
+
+    public void setSunday(Boolean sunday) {
+        this.sunday = sunday;
+    }
+}
